78d9385fc3388dd7975443dd53d5f46eab507fea
AlbanorZabeli <a.zabeli@student.avans.nl>
2024-05-07 23:41:15 +0200

    v6

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -69,14 +69,19 @@ const userService = {
         },
     
         update: (id, user, callback) => {
+            const existingUser = database._data.find(u => u.email === user.email && u.id !== id);
+
             logger.info(`update user with id ${id}`, user);
+
+            if (existingUser) {
+                logger.error('user already exists', err.message || 'unknown error');
+                callback(err, null);
+            }
+            
             database.update(id, user, (err, data) => {
                 const existingUser = database._data.find(u => u.email === user.email && u.id !== id);
                 if (err){
                     logger.error('error updating user: ', err.message || 'unknown error');
-                    callback(err, null);
-                } else if (existingUser) {
-                    logger.error('user already exists', err.message || 'unknown error');
                     callback(err, null);
                 } else {
                     logger.trace(`User updated with id ${id}.`);
