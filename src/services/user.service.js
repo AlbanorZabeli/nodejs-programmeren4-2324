98786d9a85a102bdef1de3f370b7575557addf72
AlbanorZabeli <a.zabeli@student.avans.nl>
2024-05-08 01:24:51 +0200

    Update user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -71,26 +71,27 @@ const userService = {
     update: (id, user, callback) => {
         logger.info(`Attempting to update user with id ${id}`, user);
 
-        if (database._data.find(u => u.email === u.emailAdress)) {
+        // Correct the email comparison logic and check if any user other than the current has the same email
+        if (database._data.some(u => u.email === user.email && u.id !== id)) {
             const error = new Error('A user with the same email address already exists.');
             logger.error('Error updating user: ', error.message);
             return callback(error, null);
         } else {
-        database.update(id, user, (err, data) => {
-            if (err) {
-                logger.error('Error updating user: ', err.message || 'unknown error');
-                callback(err, null);
-            }
-            else {
-                logger.trace(`User updated with id ${id}.`);
-                callback(null, {
-                    message: `User updated successfully.`,
-                    data: data
-                });
-            }
-        })};
+            database.update(id, user, (err, data) => {
+                if (err) {
+                    logger.error('Error updating user: ', err.message || 'unknown error');
+                    callback(err, null);
+                } else {
+                    logger.trace(`User updated with id ${id}.`);
+                    callback(null, {
+                        message: 'User updated successfully.',
+                        data: data
+                    });
+                }
+            });
+        }
+
     }
-    
 }
 
 module.exports = userService
