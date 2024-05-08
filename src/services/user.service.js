0cc9af9e85f4632d3b6691b61a1f3d0c68393048
AlbanorZabeli <a.zabeli@student.avans.nl>
2024-05-08 14:21:00 +0200

    Final?!

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,21 +6,27 @@ const db = require('../dao/mysql-db')
 const userService = {
     create: (user, callback) => {
         logger.info('create user', user)
-        database.add(user, (err, data) => {
-            if (err) {
-                logger.info(
-                    'error creating user: ',
-                    err.message || 'unknown error'
-                )
-                callback(err, null)
-            } else {
-                logger.trace(`User created with id ${data.id}.`)
-                callback(null, {
-                    message: `User created with id ${data.id}.`,
-                    data: data
-                })
-            }
-        })
+        if (database._data.some(u => u.emailAdress === user.emailAdress)) {
+            const errorExists = new Error('A user with the same email address already exists.');
+            logger.error('Error updating user: ', errorExists.message);
+            return callback(errorExists, null);
+        } else {
+            database.add(user, (err, data) => {
+                if (err) {
+                    logger.info(
+                        'error creating user: ',
+                        err.message || 'unknown error'
+                    )
+                    callback(err, null)
+                } else {
+                    logger.trace(`User created with id ${data.id}.`)
+                    callback(null, {
+                        message: `User created with id ${data.id}.`,
+                        data: data
+                    })
+                }
+            })
+        };
     },
 
     getAll: (callback) => {
@@ -41,12 +47,14 @@ const userService = {
     getById: (id, callback) => {
         logger.info(`getById with id ${id}`);
         database.getById(id, (err, data) => {
-            if (err) {
+            if (!data) {
+                logger.info(`User not found with id ${id}`);
+                callback(new Error('User not found with id ' + id), null);
                 logger.error('error getting user: ', err.message || 'unknown error');
                 callback(err, null);
-            } else if (!data) {
-                logger.info(`User not found with id ${id}`);
-                callback(new Error('User not found'), null);
+            } else if (err) {
+                logger.error('error getting user: ', err.message || 'unknown error');
+                callback(err, null);
             } else {
                 callback(null, {
                     message: `User found with id ${id}.`,
@@ -58,20 +66,32 @@ const userService = {
 
     delete: (id, callback) => {
         logger.info(`delete user with id ${id}`);
-        database.delete(id, (err, result) => {
-            if (err) {
-                logger.error('error deleting user: ', err.message || 'unknown error');
-                callback(err, null);
-            } else {
-                callback(null, result);
-            }
-        });
+        if (!database._data.some(u => u.id === id)) {
+            logger.info(`User not found with id ${id}`);
+            const err = new Error('No existing user was found with id: ' + id)
+            logger.error('Error updating user: ', err.message);
+            callback(err, null);
+        } else {
+            database.delete(id, (err, result) => {
+                if (err) {
+                    logger.error('error deleting user: ', err.message || 'unknown error');
+                    callback(err, null);
+                } else {
+                    callback(null, result);
+                }
+            });
+        }
     },
 
     update: (id, user, callback) => {
         logger.info(`Attempting to update user with id ${id}`, user);
-    
-        if (database._data.some(u => u.emailAdress === user.emailAdress && u.id !== id)) {
+
+        if (!database._data.some(u => u.id === id)) {
+            logger.info(`User not found with id ${id}`);
+            const err = new Error('No existing user was found with id: ' + id)
+            logger.error('Error updating user: ', err.message);
+            callback(err, null);
+        } else if (database._data.some(u => u.emailAdress === user.emailAdress && u.id !== id)) {
             const error = new Error('A user with the same email address already exists.');
             logger.error('Error updating user: ', error.message);
             return callback(error, null);
@@ -87,8 +107,9 @@ const userService = {
                         data: data
                     });
                 }
-            })};
-    } 
+            })
+        };
+    }
 }
 
 module.exports = userService
